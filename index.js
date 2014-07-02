cd2097f9d434efd343f90b723faaf270df098e21
Stanislas Polu <polu.stanislas@gmail.com>
2014-07-02 15:32:57 -0700

    Intial commit

--- index.js
+++ index.js
@@ -10,15 +10,41 @@
  */
 "use strict"
 
+var common = require('./lib/common.js');
+var async = require('async');
 var breach = require('breach_module');
 
-breach.expose('init', function(src, args, cb_) {
-  console.log('Initialization');
-});
+/******************************************************************************/
+/* MODULE BOOTSTRAP */
+/******************************************************************************/
+var bootstrap = function(http_srv) {
+  common._ = {
+    stats: require('./lib/stats.js').stats({})
+  };
 
-breach.expose('kill', function(args, cb_) {
-  common.exit(0);
-});
+  breach.register('core', 'inst:.*');
+
+  breach.expose('init', function(src, args, cb_) {
+    async.parallel([
+      common._.stats.init,
+    ], cb_);
+  });
+
+  breach.expose('kill', function(args, cb_) {
+    async.parallel([
+      common._.stats.kill,
+    ], function(err) {
+      common.exit(0);
+    });
+  });
+};
+
+/******************************************************************************/
+/* SETUP */
+/******************************************************************************/
+(function setup() {
+  return bootstrap();
+})();
 
 process.on('uncaughtException', function (err) {
   common.fatal(err);
